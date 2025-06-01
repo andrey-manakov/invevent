2039b33431cd08fbc15fcbe623777c6b065742b1
Manakov <amanakov@kept.ru>
2025-06-01 22:49:15 +0300

    .

--- config.py
+++ config.py
@@ -2,7 +2,8 @@
 import os
 from dotenv import load_dotenv
 
-load_dotenv()
+env_path = Path(__file__).resolve().parents[1] / ".env"
+load_dotenv(env_path)
 
 BOT_TOKEN: str = os.getenv("BOT_TOKEN", "")
 DB_URL: str = os.getenv("DB_URL", "sqlite:///db.sqlite3")
